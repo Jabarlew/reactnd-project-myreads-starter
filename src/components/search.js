aede57f4accf929fcdd2c5ce1b9edf6c9b143be5
Jabarlew <barlewdesign@gmail.com>
2018-07-23 02:07:53 +0200

    added function to search

--- src/components/search.js
+++ src/components/search.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import Book from './book';
-import {update, search} from 'react-router-dom';
+import {search} from '../BooksAPI';
 import { Link } from 'react-router-dom';
+
 
 class Search extends React.Component {
   state = {
-    bookSearch : []
+    searchBooks: [],
+    query: 'eeeey',
   }
 
-  
-  
+  searchBook = () =>  {
+    search(query).then((res) => {
+      this.setState({
+        searchBooks: res
+      })
+    })
+
+  }
+
+  handleQueryChange = (event) => {
+    this.setState({ query: event.taget.value });
+  }
+
     render() {
         return (
             <div className="search-books">
@@ -17,7 +30,7 @@ class Search extends React.Component {
               <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
               <div className="search-books-input-wrapper">
 
-                <input type="text" placeholder="Search by title or author"/>
+                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.handleQueryChange} />
 
               </div>
             </div>
