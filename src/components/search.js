d3e848910126dd97e796fc39a6f2e4c60519f1fe
Jabarlew <barlewdesign@gmail.com>
2018-07-23 23:41:34 +0200

    search return array wip book grid

--- src/components/search.js
+++ src/components/search.js
@@ -9,36 +9,43 @@ class Search extends React.Component {
     searchBooks: [],
     query: ''
   }
-
+  
   searchBook = () =>  {
     search(this.state.query).then((res) => {
-      this.setState({
-        searchBooks: res
-      })
+      if(res) {
+        this.setState({
+          searchBooks: res
+        })
+      }
     })
-
   }
 
   handleQueryChange = (event) => {
     this.setState({ query: event.target.value });
+    this.searchBook();
   }
 
 
 
     render() {
-        return (
+      console.log(this.state.searchBooks)
+      return (
             <div className="search-books">
             <div className="search-books-bar">
                 <Link className="close-search" to="/">Close</ Link>
               <div className="search-books-input-wrapper">
 
-                <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={this.handleQueryChange} />
-
+                <input 
+                  type="text" 
+                  placeholder="Search by title or author" 
+                  value={this.state.query} 
+                  onChange={this.handleQueryChange}
+                />
               </div>
             </div>
             <div className="search-books-results">
               <ol className="books-grid">
-
+                  
               </ol>
             </div>
           </div>
